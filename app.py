728f9e7d1b1e57d98d6336b1348d7b583a285cca
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-04-14 17:14:55 +0800

    fix bug

--- app.py
+++ app.py
@@ -90,26 +90,38 @@ pipe = pipeline(
 #     }
 # ]
 #----------------------------------------------------------------------#
-history = []
 
 
 def chat(
     query: str,
+    history: list,  # [['What is the capital of France?', 'The capital of France is Paris.'], ['Thanks', 'You are Welcome']]
     max_new_tokens: int = 1024,
     top_p: float = 0.8,
     top_k: int = 40,
     temperature: float = 0.8
 ) -> tuple[str, list]:
-    global history
     global gen_config
+
     query = query.replace(' ', '')
     if query == None or len(query) < 1:
-        # 还是返回之前的历史记录
-        for i in range(0, len(history), 2):
-            result.append((history[i]["content"], history[i+1]["content"]))
-        return result
+        return "", history
 
-    history.append(
+    # 将历史记录转换为openai格式
+    history_t = []
+    for user, assistant in history:
+        history_t.append(
+            {
+                "role": "user",
+                "content": user
+            }
+        )
+        history_t.append(
+            {
+                "role": "assistant",
+                "content": assistant
+            })
+    # 需要添加当前的query
+    history_t.append(
         {
             "role": "user",
             "content": query
@@ -125,23 +137,12 @@ def chat(
 
     # 放入 [{},{}] 格式返回一个response
     # 放入 [] 或者 [[{},{}]] 格式返回一个response列表
-    response = pipe(history, gen_config=gen_config).text
-    history.append(
-        {
-            "role": "assistant",
-            "content": response
-        })
+    response = pipe(history_t, gen_config=gen_config).text
+
     print("chat: ", query, response)
 
-    result = []
-    for i in range(0, len(history), 2):
-        result.append((history[i]["content"], history[i+1]["content"]))
-    return "", result
-
-
-def clear_history():
-    global history
-    history = []
+    history.append([query, response])
+    return "", history
 
 
 block = gr.Blocks()
@@ -158,7 +159,7 @@ with block as demo:
             # 创建聊天框
             chatbot = gr.Chatbot(height=450, show_copy_button=True)
             # 创建一个文本框组件，用于输入 prompt。
-            msg = gr.Textbox(label="Prompt/问题")
+            query = gr.Textbox(label="Prompt/问题")
 
             with gr.Row():
                 max_new_tokens = gr.Slider(
@@ -200,11 +201,9 @@ with block as demo:
 
         btn.click(
             chat,
-            inputs=[msg, max_new_tokens, top_p, top_k, temperature],
-            outputs=[msg, chatbot]
+            inputs=[query, chatbot, max_new_tokens, top_p, top_k, temperature],
+            outputs=[query, chatbot]
         )
-
-        clear.click(clear_history)
 
 # threads to consume the request
 gr.close_all()
